dc726a38c3f542ca8b25ba4c47e61c0ee7ecaad7
saebalam123 <saebalam123@gmail.com>
2023-03-14 22:30:59 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const cors = require('cors');
+var nodemailer = require('nodemailer');
+const bodyParser = require("body-parser");
+
+const PORT = process.env.PORT || 9000;
+const sgMail = require('@sendgrid/mail')
+
+const app = express();
+app.use('*', cors());
+app.use(bodyParser.json());
+
+app.post('/contactMe', (req, res) => {
+    console.log(req.body)
+    const sendername = req.body.name
+    const sendernumber = req.body.mobile
+    const senderemail = req.body.email
+    const sendermessage = req.body.message
+    let from = `${senderemail}`
+
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'saebalam123@gmail.com',
+          pass: 'lol'
+        }
+      });
+      
+      var mailOptions = {
+        from: 'temp',
+        to: 'saebalam123@gmail.com',
+        subject: 'interview intimation',
+        text: `New opportunity
+        
+            Name: ${sendername}
+            Phone: ${sendernumber}
+            Mail: ${senderemail}
+            Msg: ${sendermessage}
+            `
+      };
+      
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+          res.status(200).send("Sent successfully")
+        }
+      });
+})
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});
